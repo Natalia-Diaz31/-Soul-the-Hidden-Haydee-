f9f8d938f808b8107101ac3cf885c6ce7429308e
Sara Arévalo <20255938@esen.edu.sv>
2025-11-20 17:41:21 -0600

    Inicio.py

--- Soul and The Hidden Haydee!/Inicio.py
+++ Soul and The Hidden Haydee!/Inicio.py
@@ -1,8 +1,9 @@
-import pygame, sys#<--ayuda a cerrar el juego 
+import pygame, sys #<--ayuda a cerrar el juego 
 def mostrar_inicio(pantalla):
     background = pygame.image.load("inicio.png").convert()
     boton_comenzar = pygame.Rect(300, 656, 430, 120)
-
+# Esta sección de código genera un bucle que se reinicia cuando el jugador vuelve a acceder a la pantalla de inicio, 
+    # ya sea saliendo del juego y volviendo a entrar, o dándole clic a la pantalla para empezar a jugar.
     esperando = True
     while esperando:
         for event in pygame.event.get():
@@ -15,29 +16,18 @@ def mostrar_inicio(pantalla):
         pantalla.blit(background, (0, 0))
         pygame.display.flip()
 
-"""#Creamos ventana estableciendo sus medidas
+"""
+#Esta sección de código establece las medidas de la ventana de inicio 
 tamaño=(1038,802)
 pantalla=pygame.display.set_mode(tamaño)
 
-#le agregamos un titulo a la ventana
+#Esta sección de código mostrará título en la ventana de inicio
 pygame.display.set_caption("Soul and The Hidden Haydee!")
 fuente = pygame.font.SysFont(None, 60) #Fuente del texto
 
-# cargamos la imagen en la memoria
+# Esta sección de código carga la imagen de la pantalla en la memoria
 background=pygame.image.load("inicio.png").convert()
 
 boton_comenzar = pygame.Rect(300, 656, 430, 120)
+"""
 
-
- # Bucle de pantalla de inicio
-esperando = True
-while esperando:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                esperando = False
-
-        pantalla.blit(background, [0, 0])
-
-        pygame.display.flip()"""
